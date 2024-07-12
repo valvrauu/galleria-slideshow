4f09526c290db5796576abcc84421c6b455e9aa7
Valter Oliveira <ubi00179@gmail.com>
2024-07-12 20:27:24 -0300

    feat(styles): add initial type definition file for themes
    this allows styled components to access all properties of the current theme

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        title: string,
+
+        text: string,
+        background: string,
+    }
+}
