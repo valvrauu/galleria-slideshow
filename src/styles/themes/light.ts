0bb86541d67b5f8b364beb067e2ccc7532d833b0
Valter Oliveira <ubi00179@gmail.com>
2024-08-28 20:25:02 -0300

    feat(styles/themes): add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,99 @@
+export default {
+    title: 'light',
+
+    text: '#7D7D7D',
+    background: '#FFF',
+
+    scrollbar: {
+        track: {
+            background: '#F1F1F1',
+        },
+        thumb: {
+            background: '#A8A8A8',
+        }
+    },
+
+    header: {
+        border: {
+            bottom: '#E5E5E5'
+        }
+    },
+
+    logo: {
+        text: '#000',
+        background: 'transparent'
+    },
+
+    link: {
+        primary: {
+            color: '#7D7D7D',
+            background: 'transparent',
+            hover: {
+                color: '#000',
+                background: 'transparent',
+            }
+        },
+        secondary: {
+            color: '#7D7D7D',
+            background: 'transparent',
+            hover: {
+                color: '#000',
+                background: 'transparent',
+            }
+        }
+    },
+
+    art: {
+        name: {
+            color: '#FFF',
+            background: 'transparent'
+        },
+        artistName: {
+            color: '#FFFFFFBF',
+            background: 'transparent'
+        },
+        hover: {
+            background: '#FFFFFF80'
+        },
+        outline: 'transparent'
+    },
+
+    artDetails: {
+        captionBox: {
+            background: '#FFF',
+            name: {
+                color: '#000',
+                background: 'transparent'
+            },
+            artistName: {
+                color: '#7D7D7D',
+                background: 'transparent'
+            },
+        },
+        contentBox: {
+            background: 'transparent',
+            year: {
+                color: '#F3F3F3',
+                background: 'transparent'
+            },
+            description: {
+                color: '#7D7D7D',
+                background: 'transparent'
+            },
+        },
+        outline: 'transparent'
+    },
+
+    artNavigation: {
+        progressBar: {
+            color: '#000',
+            background: '#E5E5E5'
+        },
+        icon: {
+            color: '#000',
+            disabled: {
+                color: '#A8A8A8',
+            }
+        }
+    }
+}
