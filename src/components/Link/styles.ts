c5c078df66e3a8b8db1ab8ee73eb4e181306dc00
Valter Oliveira <ubi00179@gmail.com>
2024-08-28 21:42:29 -0300

    feat(components): add default and primary link component

---
+++ src/components/Link/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom";
+import { DefaultProps } from "../../interfaces/link";
+import { device } from "../../styles/breakpoints";
+
+export const DefaultLink = styled(Link) <DefaultProps>`
+    text-transform: ${({ transform }) => transform ?? 'none'};
+    cursor: pointer;
+
+    transition-property: color, background-color;
+    transition-duration: 250ms;
+`;
+
+export const PrimaryLink = styled(DefaultLink)`
+    color: ${({ theme }) => theme.link.primary.color};
+    background: ${({ theme }) => theme.link.primary.background};
+
+    font-size: .9rem;
+    font-weight: 700;
+    text-decoration: none;
+    letter-spacing: .214em;
+
+    &:hover {
+        color: ${({ theme }) => theme.link.primary.hover.color};
+        background: ${({ theme }) => theme.link.primary.hover.background};
+    }
+
+    @media ${device.tablet} {
+        font-size: 1.2rem;
+    }
+`;
