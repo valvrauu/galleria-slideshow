13737ece2aa3f4569e59633b656d37a75978a6a3
Valter Oliveira <ubi00179@gmail.com>
2024-08-28 21:50:58 -0300

    feat(app): create hash router with layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
-const App = () => { };
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from './providers/ThemeProvider';
+import Layout from './components/Layout';
+import { GlobalStyles } from './styles/global';
+
+const router = createHashRouter([
+    {
+        element: <Layout />,
+        children: []
+    }
+]);
+
+const App = () => {
+    return (
+        <ThemeProvider>
+            <RouterProvider router={router} />
+            <GlobalStyles />
+        </ThemeProvider>
+    );
+};
 
 export default App;
